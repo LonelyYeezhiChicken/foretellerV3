d0bbd9c8493f56bca673bb381dbac0a73a49fa1b
chicken <hibary.taiwan@gmail.com>
2022-11-22 21:05:56 +0800

    feat: CustomerController

--- Src/ff.Web/Controllers/CustomerController.cs
+++ Src/ff.Web/Controllers/CustomerController.cs
@@ -4,8 +4,16 @@ namespace ff.Web.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Go To Customer ...");
             return View();
         }
     }
