6c89e52b827eadda348254ec5fd3009a1e053d14
chicken <hibary.taiwan@gmail.com>
2022-11-22 21:27:15 +0800

    feat: CustomerApiController

--- Src/ff.Web/Controllers/WebApi/CustomerApiController.cs
+++ Src/ff.Web/Controllers/WebApi/CustomerApiController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ff.Web.Helper;
+using ff.Web.Models.ApiModel;
+using ff.Web.Models.Enum;
+using ff.Web.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ff.Web.Controllers.WebApi
@@ -7,5 +11,144 @@ namespace ff.Web.Controllers.WebApi
     [ApiController]
     public class CustomerApiController : ControllerBase
     {
+        private const string itemMain = "Customer";
+        private readonly IHttpApiHelper httpApiHelper;
+
+        public CustomerApiController()
+        {
+            httpApiHelper = new HttpApiHelper(ApiConfig.Url);
+        }
+
+        /// <summary>
+        /// 取得token
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, string> GetHeader()
+        {
+            Request.Headers.TryGetValue("APP_TOKEN", out var token);
+            return new Dictionary<string, string>()
+            {
+                {"Authorization",$"Bearer {token.First()}" }
+            };
+        }
+        /// <summary>
+        /// 檢查錯誤
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        private IActionResult GetException(Exception ex)
+        {
+            if (ex.Message.Contains("Unauthorized"))
+                return Unauthorized();
+
+            return BadRequest();
+        }
+
+        // 載入
+        [HttpGet("Load")]
+        public async Task<IActionResult> Load()
+        {
+            try
+            {
+                var header = GetHeader();
+
+                var res = await httpApiHelper.Get<List<Customer>>(itemMain, "", header);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return GetException(ex);
+            }
+        }
+
+        // 查詢
+        [HttpGet("Find")]
+        public async Task<IActionResult> Find(string kind)
+        {
+            try
+            {
+                var header = GetHeader();
+
+                var res = await httpApiHelper.Get<Customer>(itemMain, kind, header);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return GetException(ex);
+            }
+        }
+
+        // 新增
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CustomerViewModel data)
+        {
+            try
+            {
+                var header = GetHeader();
+                var res = await httpApiHelper.Post<string>(itemMain,
+                    new Customer()
+                    {
+                        Id = data.Id,
+                        Name = data.Name,
+                        CarType = data.CarType,
+                        Car = data.Car,
+                        CarNo = data.CarNo,
+                        Phone = data.Phone,
+                        Memo = data.Memo,
+                        LastLong = data.LastLong,
+                        LastDate = data.LastDate
+                    }, header);
+                return Ok("OK");
+            }
+            catch (Exception ex)
+            {
+                return GetException(ex);
+            }
+        }
+
+        // 修改
+        [HttpPost("Edit")]
+        public async Task<IActionResult> Edit(CustomerViewModel data)
+        {
+            try
+            {
+                var header = GetHeader();
+                var res = await httpApiHelper.Put<string>(itemMain,
+                    new Customer()
+                    {
+                        Id = data.Id,
+                        Name = data.Name,
+                        CarType = data.CarType,
+                        Car = data.Car,
+                        CarNo = data.CarNo,
+                        Phone = data.Phone,
+                        Memo = data.Memo,
+                        LastLong = data.LastLong,
+                        LastDate = data.LastDate
+                    }, header);
+                return Ok("OK");
+            }
+            catch (Exception ex)
+            {
+                return GetException(ex);
+            }
+        }
+
+        // 刪除
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete([FromQuery] string id)
+        {
+            try
+            {
+                var header = GetHeader();
+                var res = await httpApiHelper.Delete<string>(itemMain, $"?id={id}", header);
+                return Ok("OK");
+            }
+            catch (Exception ex)
+            {
+                return GetException(ex);
+            }
+        }
+
     }
 }
--- Src/ff.Web/Models/ApiModel/Customer.cs
+++ Src/ff.Web/Models/ApiModel/Customer.cs
@@ -2,6 +2,12 @@
 {
     public class Customer
     {
+        public Customer()
+        {
+            UpdateTime = DateTime.Now;
+            UpdateUserId = "";
+            StoreId = "";
+        }
         public string Id { get; set; }
         public string StoreId { get; set; }
         public string Name { get; set; }
