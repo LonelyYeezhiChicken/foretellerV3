7f62f9b047ee15675af4e7552c4961dfb1f037b2
chicken <hibary.taiwan@gmail.com>
2022-11-22 21:04:39 +0800

    feat: 新增會員資料檔案

---
+++ Src/ff.Web/Models/ApiModel/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace ff.Web.Models.ApiModel
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string StoreId { get; set; }
+        public string Name { get; set; }
+        public string CarType { get; set; }
+        public string Car { get; set; }
+        public string CarNo { get; set; }
+        public int Phone { get; set; }
+        public string Memo { get; set; }
+        public int LastLong { get; set; }
+        public DateTime LastDate { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public string UpdateUserId { get; set; }
+    }
+}
