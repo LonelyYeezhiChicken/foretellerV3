1850ce15435288f1e0d1a9dc533df0a9bc05c6c6
chicken <hibary.taiwan@gmail.com>
2022-11-24 22:31:07 +0800

    feat: 新增建設中畫面

---
+++ Src/ff.Web/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ff.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Create()
+        {
+            _logger.LogInformation("Go To Order Create ...");
+            return View();
+        }
+    }
+}
